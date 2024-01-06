2cb62aca178066e554c3902a063c52ad8601a9e8
Mel Mel Mom <melmelmomof4@gmail.com>
2024-01-06 17:45:34 -0500

    got the lastSearch to save to local storage

--- script.js
+++ script.js
@@ -1,69 +1,47 @@
-
-//variables
 const APIKey = 'ef2e17d0ac82b47460af0e3eef68995c';
-const city = '';
-const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-
+const searchBox = document.getElementById('searchBox');
+const day = new Date();
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const searchBtn = document.getElementById('searchBtn');
 
+// let locationIcon = document.querySelector('.weather-icon');
+// const {icon} = data.weather[0];
+// locationIcon.innerHTML = <img src="icons/${icon}.png">;
+
 const saveSearch = () => {
-  const input = {
-    search: searchBox.value,
-  }
+  const city = searchBox.value;
+  localStorage.setItem('lastSearch', city);
   console.log('click');
+}
 
 const GetInfo = () => {
-    let searchBox = document.getElementById('searchBox');
     let locationName = document.getElementById('locationName');
-    locationName.innerHTML ='--'+newName.value+'--';
+    locationName.innerHTML = '--' + searchBox.value + '--';
+  
+    const city = searchBox.value;
+// const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  const queryURL ="http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=ef2e17d0ac82b47460af0e3eef68995c";
+    fetch(queryURL)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+  
+        for (i = 0; i < 5; i++) {
+          const icon = data.weather[i].icon;
+          const iconURL = "http://openweathermap.org/img/w/10d.png";
+          document.getElementById('img' + (i + 1)).src = iconURL;
+        }
+      });
+  };
+
+const weekDay = () => {
+  if (day + d.getDay() > 6)
+    return day + d.getDay() - 7;
+  else {
+    return day + d.getDay();
+  }
 }
-
-fetch ('http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;')
-.then (response => response.json())
-.then (data => {
-    for (i = 0; i <5; i++){
-        document.getElementById ('day')
-    }
-})
-}
-
-
 
 searchBtn.addEventListener('click', function() {
-    console.log('Button clicked');
-    saveSearch();
-  });
-
-
-
-//   fetch(queryURL)
-
-
-// searchBtn.addEventListener('click', saveSearch);
-
-// //Save and display scores
-// function saveHighScore(score) {
-//     const user = {
-//         initial: initialInput.value,
-//         score: time.textContent
-//     }
-//     userScores.push(user)
-//     localStorage.setItem("userScores", JSON.stringify(userScores));
-//     initialContainer.classList.add("hide");
-//     saveHighScoreContainer.classList.remove("hide");
-//     displayHighScore()
-// }
-// function displayHighScore() {
-//     scoreList.textContent = ""
-//     for (let i = 0; i < userScores.length; i++) {
-//         var li = document.createElement("li")
-//         li.textContent = userScores[i].initial + " - " + userScores[i].score
-//         scoreList.appendChild(li);
-
-//     }
-// }
-
-
-
-
+  saveSearch();
+});

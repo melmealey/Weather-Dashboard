fae3e499e10a73a47932fb7b19d54ced0ab24580
Mel Mel Mom <melmelmomof4@gmail.com>
2024-01-07 19:09:00 -0500

    learning how to get the array to load in console

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 const searchBtn = document.getElementById('searchBtn');
 let city = searchBox.value;
 
-
-
 // let locationIcon = document.querySelector('.weather-icon');
 // const {icon} = data.weather[0];
 // locationIcon.innerHTML = <img src="icons/${icon}.png">;
@@ -17,81 +15,121 @@ const saveSearch = () => {
     console.log('click');
 }
 
-const GetInfo = () => {
-    let locationName = document.getElementById('locationName');
-    locationName.innerHTML = '--' + searchBox.value + '--';
+const GetInfo = async (url) => {
+    const response = await fetch (url);
+    const data = await response.json();
+    console.log (data);
+
+const container =document.getElementById ('weatherContainer');
+
+data.forEach((city) => {
+    const locationName = document.createElement ('div');
+    locationName.innerHTML = `$city.name`;
+    container.appendChild(locationName);
+
+    for (let i = 0; i <city.temp.length; i++) {
+        const temp = document.createElement ('div');
+        temp.innerHTML = `$city.temp`;
+        container.appendChild(temp);
+
+    }
+
+    for (let i = 0; i <city.humidity.length; i++) {
+        const humidity = document.createElement ('div');
+        temp.innerHTML = `$city.humidity`;
+        container.appendChild(humidity);
+        
+    }
+
+    for (let i = 0; i <city.windSpeed.length; i++) {
+        const windSpeed = document.createElement ('div');
+        temp.innerHTML = `city.wind`;
+        container.appendChild(windSpeed);
+        
+        console.log (data.temp)
+        console.log (data.humidity)
+        console.log (data.windSpeed)
 
 
+    }
 
-    const queryURL1 = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=ef2e17d0ac82b47460af0e3eef68995c`;
-    fetch(queryURL1)
-        .then(response1 => response1.json())
-        .then(data1 => {
-
-            const lat = data1[0].lat;
-            const lon = data1[0].lon;
-
-            console.log(data1[0].lat);
-            console.log(data1[0].lon);
-
-            const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=ef2e17d0ac82b47460af0e3eef68995c`;
-            fetch(queryURL2)
-                .then(response2 => response2.json())
-                .then(data2 => {
-                    console.log(data2);
-                })
-        })
-
-    //Save and display recent searches
-
-// Define an array to store the recent searches
-let inputCity = [];
-
-// Function to save the city search
-const saveSearches = () => {
-  const citySearch = document.getElementById('citySearch');
-  const input = {
-    city: citySearch.value,
-  };
-  inputCity.push(input);
-  localStorage.setItem("inputCity", JSON.stringify(inputCity));
-  displayRecentSearches();
-};
-
-// Function to display the recent searches
-const displayRecentSearches = () => {
-  const cityList = document.getElementById('cityList');
-  cityList.textContent = "";
-  for (let i = 0; i < inputCity.length; i++) {
-    let button = document.createElement('button');
-    button.textContent = inputCity[i].city;
-    cityList.appendChild(button);
-  }
-};
-
-// Call the displayRecentSearches function when the page loads
-displayRecentSearches();
+});
     
 };
 
+GetInfo(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=ef2e17d0ac82b47460af0e3eef68995c`);
 
 
 
-const weekDay = () => {
-    if (day + d.getDay() > 6)
-        return day + d.getDay() - 7;
-    else {
-        return day + d.getDay();
-    }
-}
+
+
+// const GetInfo = () => {
+//     let locationName = document.getElementById('locationName');
+//     locationName.innerHTML = '--' + searchBox.value + '--';
+
+//     const queryURL1 = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=ef2e17d0ac82b47460af0e3eef68995c`;
+//     fetch(queryURL1)
+//         .then(response1 => response1.json())
+//         .then(data1 => {
+
+//             const lat = data1[0].lat;
+//             const lon = data1[0].lon;
+
+//             console.log(data1[0].lat);
+//             console.log(data1[0].lon);
+
+//             const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=ef2e17d0ac82b47460af0e3eef68995c`;
+//             fetch(queryURL2)
+//                 .then(response2 => response2.json())
+//                 .then(data2 => {
+//                     console.log(data2);
+//                 })
+//         })
+
+//     //Save and display recent searches
+
+//     // Define an array to store the recent searches
+//     let inputCity = [];
+
+// };
+
+// // Function to save the city search
+// const saveSearches = () => {
+//     const citySearch = document.getElementById('citySearch');
+//     const input = {
+//         city: citySearch.value,
+//     };
+//     inputCity.push(input);
+//     localStorage.setItem("inputCity", JSON.stringify(inputCity));
+//     displayRecentSearches();
+// };
+
+// // Function to display the recent searches
+// const displayRecentSearches = () => {
+//     const cityList = document.getElementById('cityList');
+//     cityList.textContent = "";
+//     for (let i = 0; i < inputCity.length; i++) {
+//         let button = document.createElement('button');
+//         button.textContent = inputCity[i].city;
+//         cityList.appendChild(button);
+//     }
+// };
+
+// // Call the displayRecentSearches function when the page loads
+// displayRecentSearches();
+
+
+
+// const weekDay = () => {
+//     if (day + d.getDay() > 6)
+//         return day + d.getDay() - 7;
+//     else {
+//         return day + d.getDay();
+//     }
+// }
 
 searchBtn.addEventListener('click', function () {
     saveSearch();
 });
 
 
-// Around line 27, where you have the data, you want to get the latitude and longitude from it.
-// • You can see from the console.log that that will be under data[0].lat and data[0].lon , so you can do something like const lat = data[0].lat
-// • Then, you want to make a second fetch() request, just like you made the first one using those and this url. The template literal uses variables ${lat} and ${lon}to send the actual numbers as the request.
-// `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=PUTKEYHERE`
-// • After that, you can do the same handling of the data with .then(response => reponse.json()) as you did in the first request. The data there will be actual weather data, I suggest using a console.log to look inside it and see how it's structured so you can get the actual weather data out
